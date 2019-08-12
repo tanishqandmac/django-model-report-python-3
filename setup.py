c78c81be1ddd812070516a17416e48f435277fea
tanishqandmac <tanishqandmac@gmail.com>
2019-08-12 23:36:06 +0530

    yp

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ django-model-report
 
 django reports integrated with highcharts
 
-    $ git clone git://github.com/juanpex/django-model-report.git
 """
 
 
@@ -23,32 +22,27 @@ def long_description():
         return LONG_DESCRIPTION
 
 
-setup(name='django-model-report',
+setup(name='django-reports-python3',
       version=version,
-      author='juanpex',
-      author_email='jpma55@gmail.com',
+      author='tanishqandmac',
+      author_email='tanishqandmac@gmail.com',
       description='Django reports integrated with highcharts.',
-      license='BSD',
+      license='MIT',
       keywords='django, model, report, reports, highcharts, chart, charts',
-      url='https://github.com/juanpex/django-model-report',
+      url='https://github.com/tanishqandmac/django-reports-python-3',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       long_description=long_description(),
       install_requires=[
-        'django>=1.3',
         'reportlab',
         'html5lib',
-        'BeautifulSoup',
         'xhtml2pdf',
-        'xlwt==0.7.4',
+        'xlwt',
       ],
       classifiers=['Framework :: Django',
-                   'Development Status :: 3 - Alpha',
+                   'Development Status :: 1 - Beta',
                    'Topic :: Internet',
-                   'License :: OSI Approved :: BSD License',
                    'Intended Audience :: Developers',
                    'Environment :: Web Environment',
-                   'Programming Language :: Python :: 2.5',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7'])
+                   'Programming Language :: Python :: 3.6'])
